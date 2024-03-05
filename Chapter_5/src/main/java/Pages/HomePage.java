5601da6124d2a2e27da75dc33c3946f00775776b
Hedra-sami <61567779+Hedra-sami@users.noreply.github.com>
2024-03-05 02:59:23 +0200

    HoverOverPage

---
+++ Chapter_5/src/main/java/Pages/HomePage.java
@@ -0,0 +1,21 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+    By formAuthentication = By.linkText("Form Authentication");
+
+    public HomePage(WebDriver driver){
+        this.driver=driver;
+    }
+    public LoginPage clickForm(){
+        driver.findElement(formAuthentication).click();
+        return new LoginPage(driver);
+    }
+    public HoverPage gotoHoverpage(){
+        driver.findElement(By.linkText("Hovers")).click();
+        return new HoverPage(driver);
+    }
+}
